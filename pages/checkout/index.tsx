7621c8fb52ba17f736ee3cc188ca1566c3a16885
Toktar Akhmetov <tokha@gefest.dev>
2022-12-27 01:30:38 +0800

    fix

--- pages/checkout/index.tsx
+++ pages/checkout/index.tsx
@@ -1,15 +1,7 @@
 import React from "react";
-import {
-    useClaimConditions,
-    useClaimNFT,
-    useContract,
-} from "@thirdweb-dev/react";
+
 import { NextPageWithLayout } from "../_app";
-import { DesktopNavbarComponent } from "@components/navbar/navbar";
-import styles from "./page.module.css";
-import { useCartStore } from "src/store/cart.store";
-import toast from "react-hot-toast";
-import { CheckoutForm } from "@components/checkout/claim-form.component";
+
 import { DefaultLayout } from "@components/layouts/defaultLayout";
 import dynamic from "next/dynamic";
 
